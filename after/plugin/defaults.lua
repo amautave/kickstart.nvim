cdd140ba18b9391b5e461ea6b7e05b62d57ffb67
Mauricio Tabares <Mauricio.Tabares@schedulicity.com>
2023-08-13 19:42:55 -0500

    Treesitter config updated

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -3,7 +3,6 @@ vim.opt.relativenumber = true
 vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
 
 vim.keymap.set('n', '<leader>tt', '<cmd>NvimTreeToggle<cr>', { desc = '[T]ree [T]oggle' })
--- vim.keymap.set('n', '<leader>tf', '<cmd>NvimTreeFocus<cr>', { desc = '[T]ree [F]ocus' })
+vim.keymap.set('n', '<leader>tf', '<cmd>NvimTreeFocus<cr>', { desc = '[T]ree [F]ocus' })
 vim.keymap.set('n', '<leader>tr', '<cmd>NvimTreeRefresh<cr>', { desc = '[T]ree [R]efresh' })
 -- vim.keymap.set('n', '<leader>tff', '<cmd>NvimTreeFindFile<cr>', { desc = '[T]ree [F]ind [F]ile' })
-
