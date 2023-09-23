f904bde4984bb167a9ecbeded83a0233aa8411d0
Andres Tabares <andres.tabares+DFIN@dfinsolutions.com>
2023-09-22 22:51:36 -0500

    Playing with bufferline config

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -2,7 +2,8 @@ vim.opt.relativenumber = true
 
 vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
 
-vim.keymap.set('n', '<leader>et', '<cmd>Neotree toggle<cr>', { desc = '[E]xplorere [T]oggle' })
+vim.keymap.set('n', '<leader>et', '<cmd>Neotree toggle<cr>', { desc = '[E]xplorer [T]oggle' })
+vim.keymap.set('n', '<C-n>', '<cmd>Neotree toggle<cr>', { desc = 'Toggle [N]eotree' })
 vim.keymap.set('n', '<leader>ef', '<cmd>Neotree focus<cr>', { desc = '[E]xplorer [F]ocus' })
 vim.keymap.set('n', '<leader>eg', '<cmd>Neotree float git_status<cr>', { desc = '[E]xplorer [G]it Status' })
 vim.keymap.set('n', '<leader>eb', '<cmd>Neotree buffers<cr>', { desc = '[E]xplorer [B]uffers' })
@@ -18,3 +19,6 @@ vim.keymap.set('n', '<F7>', '<cmd>ToggleTermal<cr>', { desc = 'ToggleTerm' })
 -- maps.n["<leader>th"] = { "<cmd>ToggleTerm size=10 direction=horizontal<cr>", desc = "ToggleTerm horizontal split" }
 -- maps.n["<leader>tv"] = { "<cmd>ToggleTerm size=80 direction=vertical<cr>", desc = "ToggleTerm vertical split" }
 -- maps.n["<F7>"] = { "<cmd>ToggleTerm<cr>", desc = "Toggle terminal" }
+
+vim.keymap.set('n', '<Tab>', '<Cmd>BufferLineCycleNext<CR>', {})
+vim.keymap.set('n', '<S-Tab>', '<Cmd>BufferLineCyclePrev<CR>', {})
