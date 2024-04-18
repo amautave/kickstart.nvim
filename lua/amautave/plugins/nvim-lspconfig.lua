3af0941ad0aa6a22b7af34158db0a95512a9cf8f
Andres Tabares <andres.tabares+DFIN@dfinsolutions.com>
2024-04-18 11:00:29 -0500

    Format command renamed

--- lua/amautave/plugins/nvim-lspconfig.lua
+++ lua/amautave/plugins/nvim-lspconfig.lua
@@ -124,7 +124,7 @@ return {
         nmap('<leader>lr', ':LspRestart<CR>', '[L]SP [R]estart')
 
         -- Create a command `:Format` local to the LSP buffer
-        vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+        vim.api.nvim_buf_create_user_command(bufnr, 'FormatWithLSP', function(_)
           vim.lsp.buf.format()
         end, { desc = 'Format current buffer with LSP' })
       end,
